e127b87d107d315bcf26ddbe165a78e79e5cec02
rider <Riderjensen@gmail.com>
2019-11-16 18:17:09 -0700

    Simplify site

--- routes/offbrand.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const offBrandController = require('../controllers/offbrand.controller');
-
-router.get('/', offBrandController.index);
-
-module.exports = router;
